212385ae5cdd7b7b16b908270d3260d260b33574
Alvaro Gabriel <alvarogabrielgomez@gmail.com>
2021-04-28 08:56:00 -0300

    permit process.env in the host and port

--- system/core/server/index.js
+++ system/core/server/index.js
@@ -5,7 +5,7 @@ module.exports = () => {
         const server = require('./express');
         server.set('title', config.get('website:title'));
         server.listen(config.get('server:port'), config.get('server:host'), () => {
-            console.log(`🚀 - ${server.get('title')} at http://${config.get('server:host')}:${config.get('server:port')}`)
+            console.log(`🚀 - ${server.get('title')} at http://${process.env.HOST || config.get('server:host')}:${process.env.PORT || config.get('server:port')}`)
             console.log();
         });
     
